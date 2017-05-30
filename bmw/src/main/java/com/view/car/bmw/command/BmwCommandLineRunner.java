d0b6629c6c6dfe6428c2acf6c63cdd214d62f87c
yeguangkun <yeguangkun@meituan.com>
2017-05-30 17:46:24 +0800

    add bmw-dev

--- bmw/src/main/java/com/view/car/bmw/command/BmwCommandLineRunner.java
+++ bmw/src/main/java/com/view/car/bmw/command/BmwCommandLineRunner.java
@@ -4,6 +4,7 @@ import com.view.car.bmw.client.AudiClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -19,9 +20,13 @@ public class BmwCommandLineRunner implements CommandLineRunner {
     @Autowired
     private AudiClient audiClient;
 
+    @Value("${bmw.version}")
+    private String version;
+
     @Override
     public void run(String... args) throws Exception {
        String name = audiClient.audiName();
-        LOGGER.info("name:{}", name);
+       LOGGER.info("name:{}", name);
+        LOGGER.info("version:{}", version);
     }
 }
